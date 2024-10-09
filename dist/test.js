893d45add0c16b94dc06da8bbf522f09e8c02fcb
dornor <151113683+662110205@users.noreply.github.com>
2024-10-09 22:34:08 +0700

    rai wa

--- dist/test.js
+++ dist/test.js
@@ -1,13 +1,21 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.result = void 0;
 const calculate_1 = require("./calculate");
-const result = () => {
-    if (calculate_1.cal.minus(2, 1) === 1) {
+const unit_test = () => __awaiter(void 0, void 0, void 0, function* () {
+    if (calculate_1.cal.minus(3, 1) === 1) {
         console.log(0);
     }
     else {
         console.log(1);
     }
-};
-exports.result = result;
+});
+unit_test();
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,12 @@
 import { cal } from "./calculate";
 
-export const result = () =>{
-   if(cal.minus(2,1)===1){
+const unit_test = async () => {
+    if(cal.minus(3,1)===1){
         console.log(0);
     } else {
         console.log(1);
     } 
 }
+
+unit_test();
+
