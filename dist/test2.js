48762570dc687cac98f3577db9ec22faa3fcd0ef
dornor <151113683+662110205@users.noreply.github.com>
2024-10-09 23:02:57 +0700

    FINAL

--- dist/test2.js
+++ dist/test2.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const calculate_1 = require("./calculate");
-if (calculate_1.cal.plus(1, 2) === 4) {
+if (calculate_1.cal.plus(2, 2) === 4) {
     process.exit(0);
 }
 else {
--- src/test2.ts
+++ src/test2.ts
@@ -1,6 +1,6 @@
 import { cal } from "./calculate";
 
-if(cal.plus(1,2)===4){
+if(cal.plus(2,2)===4){
     process.exit(0);
 } else {
     process.exit(1);
