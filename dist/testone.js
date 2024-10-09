837c88f2a5eb13708da8b1b4c613cb694a98a37d
vannarong <151112903+vannarong@users.noreply.github.com>
2024-10-09 23:08:25 +0700

    testone3

--- dist/testone.js
+++ dist/testone.js
@@ -1,6 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__esModule1", { value: true });
-const calculate_1 = require("./calculateone");
+Object.defineProperty(exports, "__esModule", { value: true });
+const calculate_1 = require("./calculate");
 if ((0, calculateone.minus)(1, 2) === 3) {
     console.log(0);
 }
