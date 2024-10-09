27f99d7409633ef3eaf62f8266d5c6fc60914087
dornor <151113683+662110205@users.noreply.github.com>
2024-10-09 17:21:18 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+
+    res.send('I Love My Job');
+
+});
+app.listen(port, () => {
+
+    console.log(`Server is running ${port}`);
+
+});
