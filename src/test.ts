fd530e3246c51ca50d8b7e89d413400d4d3f64cb
dornor <151113683+662110205@users.noreply.github.com>
2024-10-09 22:46:31 +0700

    test

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,9 @@
 import { cal } from "./calculate";
 
-const unit_test = async () => {
-    if(cal.minus(3,1)===1){
-
-    } else {
-        console.log(1);
-    } 
+const result = cal.minus(3,1);
+if (result === 1){
+    console.log(0)
+} else {
+    console.log(1)
 }
-
-unit_test();
 
