24a882e20845c138fe7dc0e13afbffab852972f2
dornor <151113683+662110205@users.noreply.github.com>
2024-10-09 20:47:05 +0700

    i do forgor ignore

---
+++ dist/test1.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calculate_1 = require("./calculate");
+if ((0, calculate_1.minus)((0, calculate_1.multiple)(5, 6), 10) === 20) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
